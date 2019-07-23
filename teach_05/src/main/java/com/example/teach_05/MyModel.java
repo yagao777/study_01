db29103ccdc4fbffc90b7a3dd6728875e4280bb8
SanfordHuang <huangshenghuan13@gmail.com>
2019-07-23 18:34:21 +0800

    hsh:任务5 ；

--- teach_05/src/main/java/com/example/teach_05/MyModel.java
+++ teach_05/src/main/java/com/example/teach_05/MyModel.java
@@ -1,6 +1,10 @@
 package com.example.teach_05;
 
-public class MyModel {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+// 除了基本的数据类型(int,float,double,String等)，自定义的类要在Activity之间传递，必须实现序列化的接口Parcelable
+public class MyModel implements Parcelable {
 
     //======================这部分手写======================
     private String title;
@@ -20,6 +24,36 @@ public class MyModel {
     // 同时按command+N   在弹出框中选择Getter and Setter
     // 按住Command，选中所有的变量，按ok， 能够自动生成下边的方法
 
+
+    protected MyModel(Parcel in) {
+        title = in.readString();
+        subTitle = in.readString();
+        url = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(subTitle);
+        dest.writeString(url);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<MyModel> CREATOR = new Creator<MyModel>() {
+        @Override
+        public MyModel createFromParcel(Parcel in) {
+            return new MyModel(in);
+        }
+
+        @Override
+        public MyModel[] newArray(int size) {
+            return new MyModel[size];
+        }
+    };
 
     public String getTitle() {
         return title;
