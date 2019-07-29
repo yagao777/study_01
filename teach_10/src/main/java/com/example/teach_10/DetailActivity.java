978b04981b7560124024e6bf77c9526ddd71c80a
SanfordHuang <huangshenghuan13@gmail.com>
2019-07-29 21:51:12 +0700

    hsh:任务10；

---
+++ teach_10/src/main/java/com/example/teach_10/DetailActivity.java
@@ -0,0 +1,90 @@
+package com.example.teach_10;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.example.teach_10.model.MyModel;
+
+import java.text.SimpleDateFormat;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+
+
+    private ImageView ivImg2;
+    private TextView tvTitle;
+    private TextView tvSubtitle;
+    private TextView tvTime2;
+    private EditText etName;
+
+    private final static String SP_DETAIL = "sp_detail";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        ivImg2 = findViewById(R.id.iv_img2);
+        tvTitle = findViewById(R.id.tv_title);
+        tvSubtitle = findViewById(R.id.tv_subtitle);
+        tvTime2 = findViewById(R.id.tv_time2);
+        etName = findViewById(R.id.et_name);
+
+        // 这里第一个参数指的是保存的所用的sp文件的名字，一般是全局唯一的，所以应该定义一个全局常量来使用
+        // 以后在开发，一般sp文件只会有一个，这里放在这里是为了方便简单使用，正常情况下，会写一个工具类来管理
+        SharedPreferences sharedPreferences = getSharedPreferences(SP_DETAIL, DetailActivity.MODE_PRIVATE);
+        // sp保存数据是通过key-value，键值对的方式来进行保存，保存内容的时候，我们需要为内容设置一个键(key)来保存，
+        // 这里的"name"就是一个键
+        // 为什么点击进来就会崩呢？ 因为你这里把etName写在了etName = findViewById(R.id.et_name);之前
+        // 需要先拿到etName实例，才能使用它的setText
+        etName.setText(sharedPreferences.getString("et_name", ""));
+
+
+        MyModel model = getIntent().getParcelableExtra("entity");
+        tvTitle.setText(model.getTitle());
+        tvSubtitle.setText(model.getSubTitle());
+        Glide.with(this).load(model.getUrl()).into(ivImg2);
+        tvTime2.setText(format.format(model.getTime()));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_detail, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            // 应该是在点击"保存"菜单时，保存内容，
+            // 每次点击菜单时，onOptionsItemSelected方法都会被调用，我们通过菜单的id来区分是点击了什么菜单
+            // 这里点击的是id为R.id.id_preserve的菜单
+            case R.id.id_preserve:
+                // 点击保存菜单，就会走这个分支，所以应该把保存操作放在这里！！！
+                Toast.makeText(this, "success", Toast.LENGTH_SHORT).show();
+
+                // 拿到名为sp_detail的sp文件实例
+                SharedPreferences sharedPreferences = getSharedPreferences(SP_DETAIL, DetailActivity.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                // 注意，你这里定义的键为et_name ,上面你在拿的时候就应该使用et_name,而不是name
+                // 保存的内容应该是EditText的内容
+                editor.putString("et_name", etName.getText().toString());
+                editor.commit();
+                break;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ teach_10/src/main/java/com/example/teach_10/adapter/MyAdapter.java
@@ -0,0 +1,92 @@
+package com.example.teach_10.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.teach_10.R;
+import com.example.teach_10.model.MyModel;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    private List<MyModel> models;
+    private OnItemClickListener mListener;
+    private SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+
+    public MyAdapter(List<MyModel> models) {
+        this.models = models;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.adapter_my, parent, false);
+
+        return new MyViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        MyModel model = models.get(position);
+
+        Glide.with(holder.itemView.getContext()).load(model.getUrl()).into(holder.iv_img);
+        holder.tv_title.setText(model.getTitle());
+        holder.tv_subtitle.setText(model.getSubTitle());
+        holder.tv_time.setText(format.format(model.getTime()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return models.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView iv_img;
+        private final TextView tv_title;
+        private final TextView tv_subtitle;
+        private final TextView tv_time;
+
+        public MyViewHolder(@NonNull final View itemView) {
+            super(itemView);
+            iv_img = itemView.findViewById(R.id.iv_img);
+            tv_title = itemView.findViewById(R.id.tv_title);
+            tv_subtitle = itemView.findViewById(R.id.tv_subtitle);
+            tv_time = itemView.findViewById((R.id.tv_time));
+
+            // 在这里声明其他的控件
+            // ....
+
+            // 绑定Item的点击事件
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (null != mListener) {
+                        mListener.onItemClick(models.get(getAdapterPosition()));
+                    }
+                }
+            });
+        }
+    }
+
+    public void setOnItemClickListener(OnItemClickListener mListener) {
+        this.mListener = mListener;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(MyModel model);
+    }
+
+
+}
